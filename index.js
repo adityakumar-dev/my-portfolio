977d48bcba7a23273cfca9feb7e08ab27cb7af97
Aditya Kumar <adityakumar.devxlinux@gmail.com>
2024-10-25 03:25:50 +0530

    final commit for first hosting

--- index.js
+++ index.js
@@ -1,9 +1,66 @@
     const navbar = document.querySelector('header');
     const closeDiv = document.querySelector('.hamburger');
     const smallSizeLogo = document.querySelector('.hide');
-    const bio = document.querySelector('.bio');
+const bio = document.querySelector('.bio');
+
+const navbarLinks = document.querySelectorAll('.navbar a')
+
+navbarLinks.forEach(function (item) {
+    item.addEventListener('click', function (event) {
+       
+        navbarLinks.forEach(function (a) {
+            a.classList.remove('active');
+        });
+        item.classList.add('active');
+    });
+});
+
+
+const sections = document.querySelectorAll('.home, .skills, .projects, .contacts');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        const id = entry.target.getAttribute('id');
+        const correspondingLink = document.querySelector(`a[href="#${id}"]`);
+
+        if (entry.isIntersecting) {
+            navbarLinks.forEach(link => link.classList.remove('active'));
+            correspondingLink.classList.add('active');
+        }
+    });
+}, {
+    threshold: 0.5
+});
+
+const contact_me = document.querySelectorAll('.contact-me');
+
+//twitter
+contact_me[0].addEventListener('click', (event) => {
+     window.open('https://x.com/adityakumar_dev', "_blank")
+})
+//linkedin
+contact_me[1].addEventListener('click', (event) => {
+     window.open('https://www.linkedin.com/in/adityakumar-dev/', "_blank")
+})
+
+//gmail
+contact_me[2].addEventListener('click', (event) => {
+    window.open('mailto:adityakumar.devxlinux@gmail.com', "_blank");
+});
+
+
+//github
+contact_me[3].addEventListener('click', (event) => {
+     window.open('https://github.com/adityakumar-dev', "_blank")
+})
+
+sections.forEach(section => {
+    observer.observe(section);
+});
 function toggleMenu() {
-    if (navbar.style.display === 'flex') {
+    if (navbar.style.display !== 'none') {
+                navbar.style.display = 'none'
+
           for (var i = 0; i < closeDiv.children.length; i++){
               closeDiv.style = '';
               closeDiv.children[i].style = ""
@@ -13,7 +70,6 @@ function toggleMenu() {
               
         }
 
-        navbar.style.display = 'none'
     } else {
         console.log(closeDiv.children[0]);
         closeDiv.classList.remove('.hamburger')
@@ -30,7 +86,8 @@ function toggleMenu() {
   
 }
 
-  window.addEventListener('resize',   function() {
+window.addEventListener('resize', function () {
+      
       if (this.window.innerWidth < 1200) {
         // bio.style = "margin : 300px 20px;"
     }  
@@ -56,5 +113,4 @@ function toggleMenu() {
         }
         }
       
-  });
-
+});
